db74465b18e8653653265d3e6f4ac08db35286a6
skullcutter02 <=>
2021-08-10 11:22:32 +0800

    Disabled scrolling when modal is active

--- scripts/memberModal.js
+++ scripts/memberModal.js
@@ -9,9 +9,11 @@ const memberContainers = document.getElementsByClassName("member-container");
 for (let i = 0; i < memberContainers.length; i++) {
   memberContainers[i].addEventListener("click", () => {
     aboutUsModal.classList.add("enabled");
+    document.body.classList.add("stop-scrolling");
   });
 }
 
 aboutUsModalOverlay.addEventListener("click", () => {
   aboutUsModal.classList.remove("enabled");
+  document.body.classList.remove("stop-scrolling");
 });
