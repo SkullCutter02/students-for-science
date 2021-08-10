ef294061870a6e26a3acbb856689db62b6b1a4e3
skullcutter02 <=>
2021-08-10 10:33:54 +0800

    Handled basic modal anim

---
+++ scripts/memberModal.js
@@ -0,0 +1,13 @@
+const aboutUsModal = document.querySelector(".about-us-modal");
+const aboutUsModalOverlay = document.querySelector(".about-us-modal .overlay");
+const memberContainers = document.getElementsByClassName("member-container");
+
+for (let i = 0; i < memberContainers.length; i++) {
+  memberContainers[i].addEventListener("click", () => {
+    aboutUsModal.classList.add("enabled");
+  });
+}
+
+aboutUsModalOverlay.addEventListener("click", () => {
+  aboutUsModal.classList.remove("enabled");
+});
